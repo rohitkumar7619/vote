b739ce363246b551aee942b0c8d6d4e4c6349436
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-04-02 09:00:42 +0530

    adding config.js file

---
+++ backend/config.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000, // Increase timeout to 30s
+    });
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
